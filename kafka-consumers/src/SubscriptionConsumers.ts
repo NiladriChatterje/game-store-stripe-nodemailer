ae3a542495d8caa13af33cbfde919bfdd723a4ce
NiladriChatterje <radongtxgpu@gmail.com>
2025-01-25 21:53:56 +0530

    rough operation for subscription-operation is implemented. Need to check for patch operation in sanity docs

--- kafka-consumers/src/SubscriptionConsumers.ts
+++ kafka-consumers/src/SubscriptionConsumers.ts
@@ -1,10 +1,23 @@
-import { Kafka } from 'kafkajs';
+import { EachMessagePayload, Kafka } from 'kafkajs';
 import { availableParallelism } from 'node:os';
+import { createClient, SanityClient } from '@sanity/client';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const kafka = new Kafka({
     clientId: 'xv-store',
     brokers: ['localhost:9092', 'localhost:9093']
 });
+
+const sanityConfig = {
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2024-12-21',
+    useCdn: true,
+    token: process.env.SANITY_TOKEN
+}
+
+const sanityClient: SanityClient = createClient(sanityConfig);
 
 async function init() {
     const consumers = [];
@@ -13,12 +26,18 @@ async function init() {
             groupId: 'subscription-transaction',
         }));
 
+    async function handleMessage({ partition, message, topic }: EachMessagePayload) {
+        const { admin, plan } = JSON.parse(message.value.toString())
+        sanityClient.patch(plan?.document_id).insert('after', 'subscriptionPlan', [])//need to check documentation
+    }
+
     for (let consumer of consumers)
-        consumer.connect().then(async () => {
-            await consumer.subscribe({ topic: 'admin-subscription-transaction' })
+        consumer.connect().then(() => {
+            consumer.subscribe({ topic: 'admin-subscription-transaction' }).then(() => {
+                consumer.run({
+                    eachMessage: handleMessage
+                })
+            })
         })
 
-    for (let consumer of consumers) {
-
-    }
 }
