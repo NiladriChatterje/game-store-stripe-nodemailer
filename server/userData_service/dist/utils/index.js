0952643e6dab11f4714488f5d7797202e51518f1
NiladriChatterje <radongtxgpu@gmail.com>
2025-02-13 22:20:58 +0530

    ts-node nodemon => tsc-watch for userData_service

--- server/userData_service/dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const sanityConfig = {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: 'production',
-    apiVersion: '2024-12-21',
-    useCdn: true,
-    token: process.env.SANITY_TOKEN
-};
