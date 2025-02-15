6d4f10afca1da6d5b2a1df72173ba95dadc313a9
NiladriChatterje <radongtxgpu@gmail.com>
2025-02-15 12:15:13 +0530

    succummbed configuring nodemon ts-node as issue persist in resolving nodenext resolution

--- server/userData_service/src/BackgroundPingProcess.ts
+++ server/userData_service/src/BackgroundPingProcess.ts
@@ -1,18 +1,23 @@
-import { spawn } from 'node:child_process';
-import dotenv from 'dotenv';
-dotenv.config();
+import { spawn } from 'node:child_process'
+import dotenv from 'dotenv'
+dotenv.config()
 
-let old_child_process:any[]=[];
+let old_child_process: any[] = []
 setInterval(() => {
-    const child_process = spawn('curl.exe', ['-X', 'GET', `http://localhost:${process.env.PORT}/`]);
+  const child_process = spawn('curl.exe', [
+    '-X',
+    'GET',
+    `http://localhost:${process.env.PORT}/`,
+  ])
 
-    while(old_child_process.length>0){
-        let pop_process = old_child_process.pop()
-        console.log("old process killed with PID : ",pop_process.pid)
-        pop_process?.kill(0)}
+  while (old_child_process.length > 0) {
+    let pop_process = old_child_process.pop()
+    console.log('old process killed with PID : ', pop_process.pid)
+    pop_process?.kill(0)
+  }
 
-    child_process.stdout.on('data', (buffer) => {
-        console.log(buffer.toString('utf-8'));
-        old_child_process.push(child_process);
-    });
+  child_process.stdout.on('data', buffer => {
+    console.log(buffer.toString('utf-8'))
+    old_child_process.push(child_process)
+  })
 }, 60000)
--- server/userData_service/src/index.ts
+++ server/userData_service/src/index.ts
@@ -1,125 +1,119 @@
-import { Worker } from 'worker_threads';
-import cluster from 'cluster';
-import express, { Express, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { availableParallelism } from 'os';
-dotenv.config();
-
+import { Worker } from 'worker_threads'
+import cluster from 'cluster'
+import express, { Express, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { availableParallelism } from 'os'
+dotenv.config()
 
 if (cluster.isPrimary) {
-    new Worker('./dist/BackgroundPingProcess.js');
+  new Worker('./dist/BackgroundPingProcess.js')
 
-    let p;
-    for (let i = 0; i < availableParallelism(); i++) {
-        p = cluster.fork();
-        p.on('exit', (_statusCode: number) => {
-            p = cluster.fork();
-        })
-    }
-
+  let p
+  for (let i = 0; i < availableParallelism(); i++) {
+    p = cluster.fork()
+    p.on('exit', (_statusCode: number) => {
+      p = cluster.fork()
+    })
+  }
 } else {
-    const app: Express = express();
+  const app: Express = express()
 
-    app.use(cors());
-    app.use(express.json({ limit: '25mb' }));
-    app.use(express.urlencoded({ extended: true, limit: '25mb' }));
-    app.use((req: Request, res: Response, next: NextFunction) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
+  app.use(cors())
+  app.use(express.json({ limit: '25mb' }))
+  app.use(express.urlencoded({ extended: true, limit: '25mb' }))
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+  })
+
+  app.get('/', (req: Request, res: Response) => {
+    res.end('pinged!')
+  })
+
+  app.get('/fetch-user-data/:userId', (req: Request, res: Response) => {
+    console.log(req.params.userId)
+    // const NotClonedObject = {
+    //     workerData: {
+    //         value: req.body
+    //     },
+    //     transferList: req.body
+    // }
+    // const worker = new Worker('./dist/EmailWorker.js', NotClonedObject);
+    // worker.on('message', (value: boolean) => {
+    //     res.send(value);
+    // });
+    res.write('hello guys')
+  })
+
+  app.post('/razorpay', async (req: Request, res: Response) => {
+    const { price, currency } = req.body
+    console.log(price)
+    console.log(req.body)
+    const worker_razorpay = new Worker('./dist/RazorpayProcess.js', {
+      workerData: {
+        price,
+        currency,
+      },
     })
 
-    app.get('/', (req: Request, res: Response) => {
-        res.end('pinged!')
-    });
-
-    app.get('/fetch-user-data/:userId', (req: Request, res: Response) => {
-        console.log(req.params.userId);
-        // const NotClonedObject = {
-        //     workerData: {
-        //         value: req.body
-        //     },
-        //     transferList: req.body
-        // }
-        // const worker = new Worker('./dist/EmailWorker.js', NotClonedObject);
-        // worker.on('message', (value: boolean) => {
-        //     res.send(value);
-        // });
-        res.write("hello guys");
-    });
-
-    app.post("/razorpay", async (req: Request, res: Response) => {
-        const { price, currency } = req.body
-        console.log(price);
-        console.log(req.body);
-        const worker_razorpay = new Worker('./dist/RazorpayProcess.js', {
-            workerData: {
-                price, currency
-            }
-        });
-
-        worker_razorpay.on('message', (msg_event) => {
-            console.log('message : ' + msg_event);
-            res.json(msg_event)
-        })
-    });
-
-    app.post('/add-product', async (req: Request<{}, {}>, res: Response) => {
-
-        // let h = 0
-        // for (let i = 0; i < bufferArr.length; i++) {
-        //     writeFile('./dist/uploads/' + h + `.${imagesBase64[i].extension}`, bufferArr[i], 'binary', (err) => {
-        //         if (err)
-        //             console.log(err);
-        //     });
-        //     h++
-        // }
-        const worker = new Worker('./dist/ProductDetailsHandling.js')
-        worker.on('message', (data) => {
-
-        });
-        worker.postMessage(req.body, [req.body])
-        res.end('ok')
+    worker_razorpay.on('message', msg_event => {
+      console.log('message : ' + msg_event)
+      res.json(msg_event)
     })
+  })
 
-    app.post('/save-subscription', async (req: Request, res: Response) => {
-        const { adminId, admin_document_id, plan } = req.body;
-        const worker = new Worker('./dist/updateAdminSubsTransactionToDB.js', {
-            workerData: { adminId, plan },
-            transferList: [adminId, plan]
-        });
-        worker.on('message', (data) => {
-            res.json(data);
-        })
+  app.post('/add-product', async (req: Request<{}, {}>, res: Response) => {
+    // let h = 0
+    // for (let i = 0; i < bufferArr.length; i++) {
+    //     writeFile('./dist/uploads/' + h + `.${imagesBase64[i].extension}`, bufferArr[i], 'binary', (err) => {
+    //         if (err)
+    //             console.log(err);
+    //     });
+    //     h++
+    // }
+    const worker = new Worker('./dist/ProductDetailsHandling.js')
+    worker.on('message', data => {})
+    worker.postMessage(req.body, [req.body])
+    res.end('ok')
+  })
+
+  app.post('/save-subscription', async (req: Request, res: Response) => {
+    const { adminId, admin_document_id, plan } = req.body
+    const worker = new Worker('./dist/updateAdminSubsTransactionToDB.js', {
+      workerData: { adminId, plan },
+      transferList: [adminId, plan],
     })
+    worker.on('message', data => {
+      res.json(data)
+    })
+  })
 
-    app.post('/fetch-mail-otp', (req: Request, res: Response) => {
-        const OTP = Math.trunc(Math.random() * 10 ** 6);
-        const worker = new Worker('./dist/EmailWorker.js', {
-            workerData: {
-                recipient: req.body?.recipient,
-                confirmation: OTP
-            }
-        });
-        worker.on('message', (data) => {
-            if (data) res.status(200).json({ OTP });
-            else res.status(500).json({ OTP: -1 })
-        });
-    });
+  app.post('/fetch-mail-otp', (req: Request, res: Response) => {
+    const OTP = Math.trunc(Math.random() * 10 ** 6)
+    const worker = new Worker('./dist/EmailWorker.js', {
+      workerData: {
+        recipient: req.body?.recipient,
+        confirmation: OTP,
+      },
+    })
+    worker.on('message', data => {
+      if (data) res.status(200).json({ OTP })
+      else res.status(500).json({ OTP: -1 })
+    })
+  })
 
-    app.post('/fetch-phone-otp', (req: Request, res: Response) => {
-        const OTP = Math.trunc(Math.random() * 10 ** 6);
-        const worker = new Worker('./dist/PhoneWorker.js', {
-            workerData: {
+  app.post('/fetch-phone-otp', (req: Request, res: Response) => {
+    const OTP = Math.trunc(Math.random() * 10 ** 6)
+    const worker = new Worker('./dist/PhoneWorker.js', {
+      workerData: {
+        recipient: req.body?.phone,
+        confirmation: OTP,
+      },
+    })
+  })
 
-                recipient: req.body?.phone,
-                confirmation: OTP
-
-            }
-        });
-
-    });
-
-    app.listen(process.env.PORT, () => console.log('listening on PORT:' + process.env.PORT))
-
+  app.listen(process.env.PORT, () =>
+    console.log('listening on PORT:' + process.env.PORT),
+  )
 }
