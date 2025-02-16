8c5c0a56bf7419306a6346ce1425cc0c34c7c301
NiladriChatterje <radongtxgpu@gmail.com>
2025-02-17 01:41:09 +0530

    consumer for admin data upload to DB started

---
+++ kafka-consumers/src/CreateAdminConsumer.ts
@@ -0,0 +1,49 @@
+import { createClient, SanityClient } from '@sanity/client';
+import {EachMessagePayload, Kafka} from 'kafkajs';
+import { sanityConfig } from './utils';
+
+async function createAdmin(){
+    const kafka = new Kafka({
+        clientId:'xvstore',
+        brokers:["localhost:9092"]
+    });
+
+    const consumer = kafka.consumer({groupId:'admin-record'});
+    await consumer.connect();
+    await consumer.subscribe({topic:'create-admin-record'})
+
+    const sanityClient : SanityClient = createClient(sanityConfig);
+
+    
+    async function handleMessage({heartbeat,pause,message,topic,partition}:EachMessagePayload){
+        const user = JSON.parse(message.value.toString());
+        await sanityClient?.create({
+            _type:'admin',
+            adminId:user.adminId
+        })
+       
+    }
+
+    consumer.run({
+        eachMessage:handleMessage
+    })
+    
+}
+
+/*Structure sent while producing */
+// sanityClient?.create({
+//     _type: 'admin',
+//     username: user?.firstName,
+//     adminId: user?.id,
+//     email: user?.emailAddresses[0].emailAddress,
+//     geoPoint: {
+//       lat: latitude,
+//       lng: longitude,
+//     },
+//     address: {
+//       pinCode: placeResult?.properties?.postcode,
+//       county: placeResult?.properties?.county,
+//       state: placeResult?.properties?.state,
+//       country: placeResult?.properties?.country,
+//     },
+//   })
