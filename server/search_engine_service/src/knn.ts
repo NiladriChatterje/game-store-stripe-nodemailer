8ed1d47a2c91852c8ba98e4c3c5c9d9384b1e7e5
NiladriChatterje <radongtxgpu@gmail.com>
2025-05-23 22:30:41 +0530

    knn again modified

--- server/search_engine_service/src/knn.ts
+++ server/search_engine_service/src/knn.ts
@@ -2,24 +2,24 @@ function partition(arr: [string, number][], l: number, r: number) {
 
     const pivot: number = Math.trunc((l + r) / 2);
 
-    [arr[1][l], arr[1][pivot]] = [arr[1][pivot], arr[1][l]];
+    [arr[l], arr[pivot]] = [arr[pivot], arr[l]];
     l++;
 
     while (l < r) {
-        while (arr[1][l] < arr[1][pivot])
+        while (arr[l][1] < arr[pivot][1])
             l++;
 
-        while (arr[1][r] > arr[1][pivot])
+        while (arr[r][1] > arr[pivot][1])
             r--;
 
         if (l <= r) {
-            [arr[1][l], arr[1][r]] = [arr[1][r], arr[1][l]];
+            [arr[l], arr[r]] = [arr[r], arr[l]];
             l++;
             r--;
         }
     }
 
-    [arr[1][l], arr[1][r]] = [arr[1][r], arr[1][l]];
+    // [arr[l], arr[r]] = [arr[r], arr[l]];
     return l;
 }
 
@@ -36,21 +36,23 @@ function quickMerge(vectors: [string, number][], l: number = 0, r: number = vect
 export function knn(vectorEmbeddings: Map<{ toString: {}; }, { toString: {}; }> | {
     toString: {};
 }[], query: number[], k: number = 10): [string, number][] {
-    let vectorEmbeddingsMorphed: [string, number[]][];
+    const vectorEmbeddingsMorphed: [string, number[]][] = [];
 
     for (let productKey in vectorEmbeddings)
-        vectorEmbeddingsMorphed.push([productKey, vectorEmbeddings[productKey]]);
+        vectorEmbeddingsMorphed.push(vectorEmbeddings[productKey]);
 
     const result: [string, number][] = [];
     let dist;
     for (let vector of vectorEmbeddingsMorphed) {
-        dist = Math.sqrt(vector[1].reduce((acc, curr, i) => (acc + (curr[i] - query[i]) * (curr[i] - query[i])), 0));
+        dist = Math.sqrt(vector[1].reduce((acc, curr, i) => (acc + (curr - query[i]) * (curr - query[i])), 0));
         result.push([vector[0], dist])
     }
 
     quickMerge(result);
+    // result.sort((a, b) => a[1] - b[1])
 
     return result;
 }
 
-console.log(knn([[4, 3], [12, 5], [12, 12], [12, 25], [23, 25]], [4, 8]))
+console.log(knn([["6", [28, 3]], ["23", [12, 5]], ["3", [12, 12]], ["14", [12, 25]],
+["5", [23, 25]]], [4, 8]))
