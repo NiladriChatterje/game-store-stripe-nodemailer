088555fd1b6693882032bb0da1333ddab1631c70
NiladriChatterje <radongtxgpu@gmail.com>
2025-07-17 20:00:56 +0530

    UserType declaration created in KafkaConsumers microservice

---
+++ kafka-consumers/declaration/UserType.d.ts
@@ -0,0 +1,17 @@
+export declare type UserType = {
+    _id: string;
+    username: string | null | undefined;
+    geoPoint: {
+        lat: number;
+        lng: number;
+    }
+    phone?: number;
+    email: string | null | undefined;
+    address: {
+        pincode: string;
+        county: string;
+        country: string;
+        state: string;
+    },
+    cart: { _id: string; quantity: number }[]
+}
--- kafka-consumers/src/CreateUserConsumer.ts
+++ kafka-consumers/src/CreateUserConsumer.ts
@@ -1,8 +1,16 @@
-import { EachMessagePayload, Kafka, Producer } from "kafkajs";
+import { EachMessagePayload, Kafka, Consumer } from "kafkajs";
 
 const kafka = new Kafka({
     clientId: 'xv-store',
     brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094']
+});
+
+const consumer: Consumer = kafka.consumer({
+    groupId: 'user-data-creation-consumer',
+    retry: {
+        restartOnFailure: async (e: Error) => Promise.resolve(true),
+        retries: 15
+    }
 });
 async function handleMessage({ partition, topic, message, heartbeat }: EachMessagePayload) {
 
