0cec257f9578766e58216212466a7c2ebea20571
NiladriChatterje <radongtxgpu@gmail.com>
2026-06-06 22:56:00 +0530

    remove last Sanity references: fetchUserData.ts and search_engine_service/utils/index.ts

--- server/userData_service/src/fetchUserData.ts
+++ server/userData_service/src/fetchUserData.ts
@@ -1,11 +1,1 @@
-import {  SanityClient } from '@sanity/client'
-import { workerData, parentPort } from 'node:worker_threads'
-
-async function getUSerData({sanityClient,userId}:{userId:string;sanityClient:SanityClient}) {
-    const result = await sanityClient?.fetch(
-        `*[_type=='user' && userId=='${userId}']`,
-      );
-      return result;
-}
-
-getUSerData(workerData);
+export {};
